8c1b0c9ea08b921d84badb528414e4d76f527716
Crispy <cometidos.crujientes@outlook.com>
2024-11-17 11:50:12 -0800

    fixing setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install -y git
-mkdir projects && \\ 
-    cd projects && \\ 
-    git clone https://github.com/Casi-Horneado/ePaper0.git && \\ 
-    cd ePaper0 && \\
+mkdir projects && \ 
+    cd projects && \ 
+    git clone https://github.com/Casi-Horneado/ePaper0.git && \ 
+    cd ePaper0 && \
     python -m venv ePaper-venv
 
 sudo sed -i.bak 's/#dtparam=spi=on/dtparam=spi=on/g' /boot/firmware/config.>
